04848a82162eb9d0bef6eb8ae868919965613fdd
Rayan Ibrahim <rayanz6732@gmail.com>
2026-03-16 22:15:42 +0000

    File changes

--- src/pages/AnimeDetail.jsx
+++ src/pages/AnimeDetail.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { Link } from 'react-router-dom';
 import { JikanAPI } from '../lib/jikan';
