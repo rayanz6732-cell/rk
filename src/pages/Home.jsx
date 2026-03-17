f946f617250d283eeb82e85b98816f6db29cb4df
Rayan Ibrahim <rayanz6732@gmail.com>
2026-03-17 17:27:23 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,7 @@ import HeroBanner from '../components/anime/HeroBanner';
 import AnimeSection from '../components/anime/AnimeSection';
 import TrendingSidebar from '../components/anime/TrendingSidebar';
 import { Loader2, Play } from 'lucide-react';
+import SignupSection from '../components/anime/SignupSection';
 
 export default function Home() {
   const [continueWatching, setContinueWatching] = useState([]);
