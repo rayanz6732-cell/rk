c5d38dc3a161b3038f48576367391990ca48a43c
Rayan Ibrahim <rayanz6732@gmail.com>
2026-03-18 18:14:18 +0000

    File changes

--- src/pages/Watch.jsx
+++ src/pages/Watch.jsx
@@ -5,7 +5,7 @@ import { JikanAPI } from '../lib/jikan';
 import CommentsSection from '../components/anime/CommentsSection';
 import { recordWatchActivity } from '../lib/streakAndBadges';
 import { blockIframeAds } from '../lib/adBlocker';
-import AnimeKaiPlayer from '../components/anime/AnimeKaiPlayer';
+import AnimeKaiPlayer from '../components/anime/AnimeKaiPlayer.jsx';
 
 export default function Watch() {
   const [searchParams] = useSearchParams();
